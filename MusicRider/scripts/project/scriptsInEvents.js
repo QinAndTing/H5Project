283edc33ca260a4e9e077dcb19bd69dac26fcf61
QinAndTing <102577364+QinAndTing@users.noreply.github.com>
2025-02-14 19:15:58 +0800

    Add files via upload

---
+++ MusicRider/scripts/project/scriptsInEvents.js
@@ -0,0 +1,31 @@
+
+
+
+const scriptsInEvents = {
+
+	async E_loader_Event9_Act1(runtime, localVars)
+	{
+		function getFromStorage(name) {
+		    try {
+		        let val = localStorage.getItem(name);
+		        return val;
+		    } catch (e) {}
+		}
+		
+		runtime.globalVars.Saved = getFromStorage("GameSave");
+	},
+
+	async E_sys_Event24_Act1(runtime, localVars)
+	{
+		function setToStorage(name, val) {
+		    try {
+		        localStorage.setItem(name, val);
+		    } catch (e) {}
+		}
+		setToStorage("GameSave", runtime.globalVars.Saved);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
